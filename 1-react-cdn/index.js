203888016013e77c07b8b4cec46d20de21fdd349
sushantdesaur <sushantdesaur@live.in>
2022-06-25 00:18:10 +0530

    React CDN example

--- 1-react-cdn/index.js
+++ 1-react-cdn/index.js
@@ -1,17 +1,14 @@
 
-const Navbar = () => {
-    return (
-    <nav>
-        <ul>
-            <li>List Item 1</li>
+const Navbar = (
+        <nav>
+            <ul>
+                <li>List Item 1</li>
             <li>List Item 2</li>
         </ul>
     </nav>
-    )
-};
-
+)
+ 
+console.log(Navbar)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
- <Navbar/>
-);
+root.render(Navbar);
