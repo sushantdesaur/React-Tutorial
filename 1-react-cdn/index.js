f084d66e6fcff9f5eef3170b392607024b279a28
sushantdesaur <sushantdesaur@live.in>
2022-06-25 00:08:40 +0530

    React CDN

---
+++ 1-react-cdn/index.js
@@ -0,0 +1,17 @@
+
+const Navbar = () => {
+    return (
+    <nav>
+        <ul>
+            <li>List Item 1</li>
+            <li>List Item 2</li>
+        </ul>
+    </nav>
+    )
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+ <Navbar/>
+);
