2050eba34bcd98eb6fed289bed632f7466bd44ec
sushantdesaur <sushantdesaur@live.in>
2022-06-25 01:18:07 +0530

    Created local react app * Used yarn package manager * yarn create react-app

--- 1-react-cdn/index.js
+++ 1-react-cdn/index.js
@@ -1,14 +1,14 @@
 
 const Navbar = (
-        <nav>
-            <ul>
-                <li>List Item 1</li>
-            <li>List Item 2</li>
-        </ul>
-    </nav>
-)
- 
-console.log(Navbar)
+  <nav>
+    <ul>
+      <li>List Item 1</li>
+      <li>List Item 2</li>
+    </ul>
+  </nav>
+);
+
+console.log(Navbar);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(Navbar);
